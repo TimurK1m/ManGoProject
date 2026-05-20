06926201c83761cf28753628494aeb84ba14d3bb
eduardovichmsc <eduardovdarzhan@gmail.com>
2026-05-21 01:48:53 +0500

    added: cors

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,7 @@ import (
 	"manGo/internal/handlers"
 	workers "manGo/internal/workers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -33,6 +34,15 @@ func main() {
 
     
     r := gin.Default()
+
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge: 12 * time.Hour,
+    }))
 
     
     handlers.RegisterRoutes(r, db, cfg)
